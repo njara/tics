f02d6cf2f330ddf92bc7bf5f5dc711ba9d5c8102
nicolasjara <nicolas@bitbuckte.org>
2016-12-12 00:19:07 -0300

    espacio botones

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -70,6 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				<p>ALUMNO</p>
 				
 				<a class="btn btn-default" href="<?php echo base_url();?>index.php/usuarios/iniciar_sesion">Iniciar Sesión Alumno</a>
+				<br>
 				<a class="btn btn-default" href="<?php echo base_url();?>index.php/usuarios/registro_usuario">Crear Cuento Alumno</a>
 			</div>
 		</div>
